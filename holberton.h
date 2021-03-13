e5a857aaa5b777f2a3b3ec4f96fb536b49d7fa2c
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-13 18:05:51 +0000

    finished header file for now

--- holberton.h
+++ holberton.h
@@ -1,16 +1,23 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+/* Libraries to use  */
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stddef.h>
 
+/* Struct definition named param_manager, type: param  */
 #typedef struct param_manager
 {
 	char *param;
 	void (*f)(va_list args);
 } param;
 
+/* Prototypes  */
+int _printf(char *str, ...);
+void print_int(int n, int *count_p);
+void print_str(char *str, int *count_p);
 
-#endif
+
+#endif /* HEADER_H */
