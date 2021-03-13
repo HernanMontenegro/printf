8dd88762236ed011d4a4cd9ec43f2a469df86938
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-13 19:32:07 +0000

    uppercase some comments

--- _printf.c
+++ _printf.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
-* _printf - the giant printf programmed by 2 guys very hard!!
-* @str: basic string parameter
-* @...: the unknown amount of parameters representation
+* _printf - The giant printf programmed by 2 guys very hard!!
+* @str: Basic string parameter
+* @...: The unknown amount of parameters representation
 * -------------------------------------------
-* Return: amount of characters printed, -1 if fails
+* Return: Amount of characters printed, -1 if fails
 */
 int _printf(char *str, ...)
 {
@@ -22,19 +22,20 @@ int _printf(char *str, ...)
 
 	va_start(args, str);
 
+	/* Checks if str given is NULL  */
 	if (!str)
 		return (-1);
 
 	for (i = 0; str && str[i]; i++)
 	{
-		/* if current char isn't '%' print normally  */
+		/* If current char isn't '%' print normally  */
 		if (str[i] != '%')
 		{
 			_putchar(str[i]);
 			*count_p = *count_p + 1;
 			continue;
 		}
-		/* this for will execute if current char is '%'  */
+		/* This for will execute if current char is '%'  */
 		for (j = 0; j < 4; j++)
 		{
 			if (*params[j].param == str[i + 1])
