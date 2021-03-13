ecbd1dc269e54538a398ea229801411069d0270c
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-13 16:54:32 +0000

    header file with struct and some libraries

--- holberton.h
+++ holberton.h
@@ -1,7 +1,16 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
 
+#typedef struct param_manager
+{
+	char *param;
+	void (*f)(va_list args);
+} param;
 
 
 #endif
