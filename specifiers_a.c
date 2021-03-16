620ccbc3939da5c90c0821b70574403b2c72cc52
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-16 17:21:03 +0000

    meme

--- specifiers_a.c
+++ specifiers_a.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 
-
 /**
- * binary_process - prints an int in binary and keeps count
- * @n: Number to print
- * @p: pointer to count variable
- * @bool: checks if is the first call
- *
- * Return: void
- */
+* binary_process - prints an int in binary and keeps count
+* @n: Number to print
+* @p: pointer to count variable
+* @bool: checks if is the first call
+*
+* Return: void
+*/
 void binary_process(unsigned int n, int *p)
 {
 	unsigned int m;
@@ -23,14 +22,14 @@ void binary_process(unsigned int n, int *p)
 }
 
 /**
- * print_binary - supply the value to the print function
- * @args: List to get va_arg
- * @p: pointer to count variable
- *
- */
+* print_binary - supply the value to the print function
+* @args: List to get va_arg
+* @p: pointer to count variable
+*
+*/
 void print_binary(va_list args, int *p)
 {
-	int n = va_arg(args, unsigned int);
+	unsigned int n = va_arg(args, unsigned int);
 
 	binary_process(n, p);
 }
